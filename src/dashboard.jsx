cc44cb5751e380e5fdab3cfebd4c220c44a737b0
Omar Salem <omarsalem0721@gmail.com>
2022-10-04 11:57:46 +0200

    Create react app with vite

---
+++ src/dashboard.jsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <div className="App">hello world</div>;
+}
+
+export default Dashboard;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Dashboard from './dashboard';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>
+);
